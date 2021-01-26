faba0da2240090766e14c0bf3eca8713e235ae84
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2021-01-26 14:13:24 +0200

    Controller

--- DepartmentService/src/main/java/com/example/demo/Controller/DepartmentController.java
+++ DepartmentService/src/main/java/com/example/demo/Controller/DepartmentController.java
@@ -1,34 +1,43 @@
 package com.example.demo.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Models.Department;
 import com.example.demo.Service.MyService;
 
 @RestController
+@RequestMapping("/departments")
 public class DepartmentController {
 	
 	@Autowired
 	private MyService myService;
 	
-	
-	public Department saveDepartment(Department department) {
+	@PostMapping("/save")
+	public Department saveDepartment(@RequestBody Department department) {
 		return myService.saveDepartment(department);
 	}
 	
+	@GetMapping("/")
 	public List<Department>  getAllDepartment(){
 		return myService.getAllDepartment();
 	}
 	
-	public Department findDepartmentById(int id) {
+	@GetMapping("/{id}")
+	public Department findDepartmentById(@PathVariable int id) {
 		return myService.findDepartmentById(id);
 	}
 	
-	public String deleteDepartment(int id) {
+	@DeleteMapping("/{id}")
+	public String deleteDepartment(@PathVariable int id) {
 		myService.deleteDepartment(id);
 		return "Department deleted successfuly";
 	}
